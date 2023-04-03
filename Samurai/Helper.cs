d5bf4fc52919b85e7ccb36dd2eb66e4080cc8ee5
Sbago <ianljl@163.com>
2023-04-03 17:18:01 +0800

    Update Helper.cs

--- Samurai/Helper.cs
+++ Samurai/Helper.cs
@@ -33,6 +33,7 @@ namespace Samurai
                 //感谢gamous 提供 582-冷却中 581-演出限制（无buff） 579-状态限制（有buff） 573-未学会（包括等级不足） 572-技能附加条件未满足（忍者忍术条件有另外的判定） 568-没蓝 580-咏唱中 0 可用 
                 0 => true,
                 572 or 580 or 568 or 573 or 581 or 579 => false,
+                //todo 应该进步判断有cd的gcd技能问题
                 582 => ((ActionData*)Core.Get<IMemApiFunctionPointer>().GetActionData(actionId))->IsGCD() && Core.Get<IMemApiFunctionPointer>().IsCanQueue(actionId),
                 _ => false,
             } ;
